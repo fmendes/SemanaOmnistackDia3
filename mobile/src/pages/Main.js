fb8285b990e68f85defdf3af519aa3f917b7f7f6
Fernando Fernandez <fmendes@email.com>
2020-01-17 00:16:36 -0600

    Yawn!

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View
+    , Text, TouchableOpacity, Keyboard } from 'react-native';
 //import { View } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { TextInput } from 'react-native-gesture-handler';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main( { navigation } ) {
     const [ currentRegion, setCurrentRegion ] = useState( null );
@@ -35,6 +38,7 @@ function Main( { navigation } ) {
     }
 
     return ( 
+    <>
     <MapView initialRegion={currentRegion}
                     style={styles.map} >
         <Marker coordinate={{ latitude: 37.7780959, longitude: -122.4079491 }} >
@@ -52,9 +56,29 @@ function Main( { navigation } ) {
             </Callout>
         </Marker>
     </MapView>
+    <View style={styles.searchForm} >
+        <TextInput style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                    />
+        <TouchableOpacity onPress={ () => {
+
+        } }
+                    style={styles.loadButton} >
+            <MaterialIcons name="my-location"
+                            size={20}
+                            color="white" />
+        </TouchableOpacity>
+                        
+    </View>
+    </>
     );
     //return <View />
 }
+
+// TODO:  use Keyboard api to detect keyboard and move search input up
 
 const styles = StyleSheet.create({
     map: {
@@ -80,6 +104,39 @@ const styles = StyleSheet.create({
     }
     , devTechs: {
         marginTop: 5
+    }
+    , searchForm: {
+        position: 'absolute'
+        , bottom: 20
+        , left: 20
+        , right: 20
+        , zIndex: 5
+        , flexDirection: 'row'
+    }
+    , searchInput: {
+        flex: 1
+        , height: 50
+        , backgroundColor: 'white'
+        , color: "#333"
+        , borderRadius: 25
+        , paddingHorizontal: 20
+        , fontSize: 16
+        , shadowColor: 'black'
+        , shadowOpacity: 0.2
+        , shadowOffset: {
+            width: 4
+            , height: 4 
+        }
+        , elevation: 2
+    }
+    , loadButton: {
+        width: 50
+        , height: 50
+        , backgroundColor: '#8e4dff'
+        , borderRadius: 25
+        , justifyContent: 'center'
+        , alignItems: 'center'
+        , marginLeft: 15
     }
   });
 
